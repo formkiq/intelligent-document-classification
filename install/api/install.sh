670a06a40c5b47cdfd4363c1d3bbb63c1ea823b9
Mike Friesen - FormKiQ <mike@formkiq.com>
2023-04-06 21:52:59 -0700

    updates

--- install/api/install.sh
+++ install/api/install.sh
@@ -25,11 +25,27 @@ docker-compose -f ../../docker-compose-prod.yml run --rm --entrypoint "\
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 
+docker-compose -f ../../docker-compose-prod.yml build --build-arg SERVER_NAME="app.${IP_PUBLIC}.nip.io"
 docker-compose -f ../../docker-compose-prod.yml up -d
 
 sudo rm -r -f /etc/letsencrypt/
 docker run -it --rm -v /var/www/certbot/:/var/www/certbot/ -v /etc/letsencrypt/:/etc/letsencrypt/ certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/var/www/certbot/ -d "app.${IP_PUBLIC}.nip.io"
 
-docker-compose down
+if [ $? -eq 0 ] 
+then 
+  echo "Successfully created domain certificate"
+  docker-compose -f ../../docker-compose-prod.yml down 
+else 
+  docker-compose -f ../../docker-compose-prod.yml down
+  echo "Defaulting to Self-signed certificate"
+  sudo rm -r -f /etc/letsencrypt/
+  sudo mkdir -p "$path"
 
-docker-compose -f ../../docker-compose-prod.yml build --build-arg SERVER_NAME="app.${IP_PUBLIC}.nip.io" --no-cache
+  docker-compose -f ../../docker-compose-prod.yml run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:4096 -days 1000\
+      -keyout '$path/privkey.pem' \
+      -out '$path/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
+fi
+
+docker-compose -f ../../docker-compose-prod.yml up -d
