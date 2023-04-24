b42f2b7e7c80a3d437c6d2be1c40758851343d25
Mike Friesen - FormKiQ <mike@formkiq.com>
2023-04-23 21:20:08 -0500

    removed sudo

--- install/api/install.sh
+++ install/api/install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-sudo yum -y update
+yum -y update
 
-sudo yum -y install docker git
+yum -y install docker git
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # git clone https://github.com/formkiq/intelligent-document-classification.git
 #usermod -a -G docker ${USER}
 #newgrp docker
 
-sudo systemctl enable docker.service
+systemctl enable docker.service
 
-sudo systemctl start docker.service
+systemctl start docker.service
 
-sudo rm -r -f /etc/letsencrypt/
+rm -r -f /etc/letsencrypt/
 path="/etc/letsencrypt/live/app.${IP_PUBLIC}.nip.io"
-sudo mkdir -p "$path"
+mkdir -p "$path"
 
 docker-compose -f ../../docker-compose-prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:4096 -days 1000\
@@ -29,7 +29,7 @@ docker-compose -f ../../docker-compose-prod.yml run --rm --entrypoint "\
 docker-compose -f ../../docker-compose-prod.yml build --build-arg SERVER_NAME="app.${IP_PUBLIC}.nip.io"
 docker-compose -f ../../docker-compose-prod.yml up -d
 
-sudo rm -r -f /etc/letsencrypt/
+rm -r -f /etc/letsencrypt/
 docker run -it --rm -v /var/www/certbot/:/var/www/certbot/ -v /etc/letsencrypt/:/etc/letsencrypt/ certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/var/www/certbot/ -d "app.${IP_PUBLIC}.nip.io"
 
 if [ $? -eq 0 ] 
@@ -39,8 +39,8 @@ then
 else 
   docker-compose -f ../../docker-compose-prod.yml down
   echo "Defaulting to Self-signed certificate"
-  sudo rm -r -f /etc/letsencrypt/
-  sudo mkdir -p "$path"
+  rm -r -f /etc/letsencrypt/
+  mkdir -p "$path"
 
   docker-compose -f ../../docker-compose-prod.yml run --rm --entrypoint "\
     openssl req -x509 -nodes -newkey rsa:4096 -days 1000\
