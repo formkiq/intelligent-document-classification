1bac16223e11af414dadb175283c3ee54961b685
Mike Friesen - FormKiQ <mike@formkiq.com>
2023-04-24 22:44:26 -0500

    removed unused code

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,23 +13,13 @@ import { HomeLayout } from "./components/HomeLayout";
 import "./styles.css";
 import { AuthLayout } from "./components/AuthLayout";
 
-// ideally this would be an API call to server to get logged in user data
-
 const getUserData = () =>
   new Promise((resolve) =>
     setTimeout(() => {
       const user = window.localStorage.getItem("user");
       resolve(user);
-    }, 3000)
+    }, 100)
   );
-
-// for error
-// const getUserData = () =>
-//   new Promise((resolve, reject) =>
-//     setTimeout(() => {
-//       reject("Error");
-//     }, 3000)
-//   );
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
