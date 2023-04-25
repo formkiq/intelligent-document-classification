d57763fac71ca5fe9c6416969dd25d0b09bf9ed8
Mike Friesen - FormKiQ <mike@formkiq.com>
2023-04-24 22:16:10 -0500

    removed sudo

--- install/elasticsearch/install.sh
+++ install/elasticsearch/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo yum -y update
+yum -y update
 
-sudo amazon-linux-extras enable java-openjdk11
+amazon-linux-extras enable java-openjdk11
 
-sudo yum -y install java-11-openjdk
+yum -y install java-11-openjdk
 
-cat <<EOF | sudo tee /etc/yum.repos.d/elasticsearch.repo
+cat <<EOF | tee /etc/yum.repos.d/elasticsearch.repo
 [elasticsearch-8.x]
 name=Elasticsearch repository for 8.x packages
 baseurl=https://artifacts.elastic.co/packages/8.x/yum
@@ -17,30 +17,30 @@ autorefresh=1
 type=rpm-md
 EOF
 
-sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-sudo yum clean all
+yum clean all
 
-sudo yum makecache
+yum makecache
 
-sudo yum -y install elasticsearch
+yum -y install elasticsearch
 
-sudo sed -i '/xpack.security.enabled/s/: .*/: false/' /etc/elasticsearch/elasticsearch.yml
-echo 'discovery.seed_hosts: []' | sudo tee -a /etc/elasticsearch/elasticsearch.yml
-echo 'network.host: 0.0.0.0' | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sed -i '/xpack.security.enabled/s/: .*/: false/' /etc/elasticsearch/elasticsearch.yml
+echo 'discovery.seed_hosts: []' | tee -a /etc/elasticsearch/elasticsearch.yml
+echo 'network.host: 0.0.0.0' | tee -a /etc/elasticsearch/elasticsearch.yml
 
-sudo systemctl restart elasticsearch
+systemctl restart elasticsearch
 
-sudo systemctl enable elasticsearch
+systemctl enable elasticsearch
 
 # kibana
-sudo yum -y install kibana
+yum -y install kibana
 
-echo 'server.port: 5601' | sudo tee -a /etc/kibana/kibana.yml
-echo 'server.host: "0.0.0.0"' | sudo tee -a /etc/kibana/kibana.yml
-echo 'elasticsearch.hosts: ["http://127.0.0.1:9200"]' | sudo tee -a /etc/kibana/kibana.yml
+echo 'server.port: 5601' | tee -a /etc/kibana/kibana.yml
+echo 'server.host: "0.0.0.0"' | tee -a /etc/kibana/kibana.yml
+echo 'elasticsearch.hosts: ["http://127.0.0.1:9200"]' | tee -a /etc/kibana/kibana.yml
 
-echo "server.publicBaseUrl: \"http://${IP_PUBLIC}:5601\"" | sudo tee -a /etc/kibana/kibana.yml
+echo "server.publicBaseUrl: \"http://${IP_PUBLIC}:5601\"" | tee -a /etc/kibana/kibana.yml
 
-sudo systemctl restart kibana
-sudo systemctl enable kibana
+systemctl restart kibana
+systemctl enable kibana
