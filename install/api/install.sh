e7062cd61ee240dd0588f797be7847fc6981f31b
Mike Friesen <mike@Blurr.local>
2023-04-28 21:56:18 -0500

    update

--- install/api/install.sh
+++ install/api/install.sh
@@ -8,8 +8,9 @@ curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compos
 
 chmod +x /usr/local/bin/docker-compose
 
-git clone https://github.com/formkiq/intelligent-document-classification.git
-# git checkout --track origin/v1
+git clone https://github.com/formkiq/intelligent-document-classification.git ~
+cd ~/intelligent-document-classification
+git checkout --track origin/v1
 
 # systemctl enable docker.service
 
@@ -19,14 +20,14 @@ git clone https://github.com/formkiq/intelligent-document-classification.git
 # path="/etc/letsencrypt/live/app.${IP_PUBLIC}.nip.io"
 # mkdir -p "$path"
 
-# docker-compose -f /usr/local/bin/docker-compose-prod.yml run --rm --entrypoint "\
+# docker-compose -f docker-compose-prod.yml run --rm --entrypoint "\
 #   openssl req -x509 -nodes -newkey rsa:4096 -days 1000\
 #     -keyout '$path/privkey.pem' \
 #     -out '$path/fullchain.pem' \
 #     -subj '/CN=localhost'" certbot
 
-# docker-compose -f /usr/local/bin/docker-compose-prod.yml build --build-arg SERVER_NAME="app.${IP_PUBLIC}.nip.io"
-# docker-compose -f /usr/local/bin/docker-compose-prod.yml up -d
+# docker-compose -f docker-compose-prod.yml build --build-arg SERVER_NAME="app.${IP_PUBLIC}.nip.io"
+# docker-compose -f docker-compose-prod.yml up -d
 
 # rm -r -f /etc/letsencrypt/
 # docker run -it --rm -v /var/www/certbot/:/var/www/certbot/ -v /etc/letsencrypt/:/etc/letsencrypt/ certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/var/www/certbot/ -d "app.${IP_PUBLIC}.nip.io"
@@ -34,18 +35,18 @@ git clone https://github.com/formkiq/intelligent-document-classification.git
 # if [ $? -eq 0 ] 
 # then 
 #   echo "Successfully created domain certificate"
-#   docker-compose -f /usr/local/bin/docker-compose-prod.yml down 
+#   docker-compose -f docker-compose-prod.yml down 
 # else 
-#   docker-compose -f /usr/local/bin/docker-compose-prod.yml down
+#   docker-compose -f docker-compose-prod.yml down
 #   echo "Defaulting to Self-signed certificate"
 #   rm -r -f /etc/letsencrypt/
 #   mkdir -p "$path"
 
-#   docker-compose -f /usr/local/bin/docker-compose-prod.yml run --rm --entrypoint "\
+#   docker-compose -f docker-compose-prod.yml run --rm --entrypoint "\
 #     openssl req -x509 -nodes -newkey rsa:4096 -days 1000\
 #       -keyout '$path/privkey.pem' \
 #       -out '$path/fullchain.pem' \
 #       -subj '/CN=localhost'" certbot
 # fi
 
-# docker-compose -f /usr/local/bin/docker-compose-prod.yml up -d
+# docker-compose -f docker-compose-prod.yml up -d
