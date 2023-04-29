12effb42907a554b4420d7828e56b1db80721557
Mike Friesen <mike@Blurr.local>
2023-04-28 22:41:08 -0500

    update

--- install/api/install.sh
+++ install/api/install.sh
@@ -13,23 +13,30 @@ git clone https://github.com/formkiq/intelligent-document-classification.git ~/i
 cd ~/intelligent-document-classification
 git checkout --track origin/v1
 
+echo "Enable Docker Service"
 systemctl enable docker.service
 
+echo "Starting Docker Service"
 systemctl start docker.service
 
 rm -r -f /etc/letsencrypt/
 path="/etc/letsencrypt/live/app.${IP_PUBLIC}.nip.io"
 mkdir -p "$path"
 
+echo "Generating Self Signed Certificate"
 docker-compose -f docker-compose-prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:4096 -days 1000\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 
+echo "Building Docker Project"
 docker-compose -f docker-compose-prod.yml build --build-arg SERVER_NAME="app.${IP_PUBLIC}.nip.io"
+
+echo "Launching Docker Project"
 docker-compose -f docker-compose-prod.yml up -d
 
+echo "Generating Lets Encrypt Certificate"
 rm -r -f /etc/letsencrypt/
 docker run -it --rm -v /var/www/certbot/:/var/www/certbot/ -v /etc/letsencrypt/:/etc/letsencrypt/ certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/var/www/certbot/ -d "app.${IP_PUBLIC}.nip.io"
 
@@ -50,4 +57,5 @@ else
       -subj '/CN=localhost'" certbot
 fi
 
+echo "Launching Intelligent Document Classification Application"
 docker-compose -f docker-compose-prod.yml up -d
