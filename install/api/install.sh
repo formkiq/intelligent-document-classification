491b0e73f4a22c6ceb70515ab9ef76cfb59360ec
Mike Friesen <mike@Blurr.local>
2023-04-29 21:59:09 -0500

    update

--- install/api/install.sh
+++ install/api/install.sh
@@ -39,14 +39,15 @@ echo "Generating Lets Encrypt Certificate"
 rm -r -f /etc/letsencrypt/
 docker run -it --rm -v /var/www/certbot/:/var/www/certbot/ -v /etc/letsencrypt/:/etc/letsencrypt/ certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/var/www/certbot/ -d "app.${IP_PUBLIC}.nip.io"
 
-# if [ ! -f "${path}/fullchain.pem" ]; then
-#   echo "Defaulting to Self-signed certificate"
-#   rm -r -f /etc/letsencrypt/
-#   mkdir -p "$path"
-#   openssl req -x509 -nodes -newkey rsa:4096 -days 1000 -keyout "${path}/privkey.pem" -out "${path}/fullchain.pem" -subj "/CN=localhost"
-# fi
+if [ ! -f "${path}/fullchain.pem" ]; then
+  echo "Defaulting to Self-signed certificate"
+  rm -r -f /etc/letsencrypt/
+  mkdir -p "$path"
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1000 -keyout "${path}/privkey.pem" -out "${path}/fullchain.pem" -subj "/CN=localhost"
+fi
 
-# docker-compose -f docker-compose-prod.yml down 
+cd ~/intelligent-document-classification
+docker-compose -f docker-compose-prod.yml down 
 
 echo "Launching Intelligent Document Classification Application"
-# docker-compose -f docker-compose-prod.yml up -d
+docker-compose -f docker-compose-prod.yml up -d
