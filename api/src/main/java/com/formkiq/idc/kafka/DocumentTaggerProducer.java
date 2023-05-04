be40ebcf837e8bbf474d6c5e924de0f97ee83e94
Mike Friesen <mike@Blurr.local>
2023-05-04 13:52:31 -0500

    increase kafka step timeout to 60 minutes

--- api/src/main/java/com/formkiq/idc/kafka/DocumentTaggerProducer.java
+++ api/src/main/java/com/formkiq/idc/kafka/DocumentTaggerProducer.java
@@ -1,9 +1,13 @@
 package com.formkiq.idc.kafka;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+
 import io.micronaut.configuration.kafka.annotation.KafkaClient;
 import io.micronaut.configuration.kafka.annotation.Topic;
+import io.micronaut.context.annotation.Property;
 
-@KafkaClient
+@KafkaClient(acks = KafkaClient.Acknowledge.ALL, properties = {
+		@Property(name = ProducerConfig.RETRIES_CONFIG, value = "0") })
 public interface DocumentTaggerProducer {
 
 	@Topic("document_tagging")
--- api/src/main/java/com/formkiq/idc/kafka/TesseractProducer.java
+++ api/src/main/java/com/formkiq/idc/kafka/TesseractProducer.java
@@ -1,10 +1,14 @@
 package com.formkiq.idc.kafka;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+
 import io.micronaut.configuration.kafka.annotation.KafkaClient;
 import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.Topic;
+import io.micronaut.context.annotation.Property;
 
-@KafkaClient
+@KafkaClient(acks = KafkaClient.Acknowledge.ALL, properties = {
+		@Property(name = ProducerConfig.RETRIES_CONFIG, value = "0") })
 public interface TesseractProducer {
 
 	@Topic("tesseract")
