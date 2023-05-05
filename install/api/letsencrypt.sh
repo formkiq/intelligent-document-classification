53153300d6fc4d1097769c9e21be0f7653df8d5f
Mike Friesen <mike@Blurr.local>
2023-05-04 21:36:40 -0500

    update

--- install/api/letsencrypt.sh
+++ install/api/letsencrypt.sh
@@ -4,6 +4,7 @@ echo "Generating Lets Encrypt Certificate"
 rm -r -f /etc/letsencrypt/
 docker run --rm -v /var/www/certbot/:/var/www/certbot/ -v /etc/letsencrypt/:/etc/letsencrypt/ certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/var/www/certbot/ -d "app.${IP_PUBLIC}.nip.io"
 
+path="/etc/letsencrypt/live/app.${IP_PUBLIC}.nip.io"
 if [ ! -f "${path}/fullchain.pem" ]; then
   echo "Defaulting to Self-signed certificate"
   rm -r -f /etc/letsencrypt/
