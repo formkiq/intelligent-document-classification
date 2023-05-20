d98ea3fc439366edbaf44a11f40cad4989e54863
Mike Friesen <mfriesen@gmail.com>
2023-05-20 09:15:46 -0500

    Added "restart.sh" script and descriptive comments to the other scripts

--- install/api/restart.sh
+++ install/api/restart.sh
@@ -2,9 +2,11 @@
 
 #####################################################################
 #
-# This script Starts the Intelligent Document Classification API
+# This script Restarts the Intelligent Document Classification API
 #
 #####################################################################
 
 cd ~/intelligent-document-classification
+
+docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml up -d
--- install/api/stop.sh
+++ install/api/stop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#####################################################################
+#
+# This script STOPS the Intelligent Document Classification API
+#
+#####################################################################
+
 cd ~/intelligent-document-classification
 docker-compose -f docker-compose-prod.yml down
